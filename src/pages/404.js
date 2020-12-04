9e3d6a8346a858d986225036735431ae10eef924
Arno van Staden <arnovanstaden@gmail.com>
2020-12-04 15:29:34 +0200

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,18 @@
-// import React from "react";
-// import Layout from "../components/Layout";
+import React from "react";
+import Layout from "../components/Layout";
 
-// const NotFound = () => {
-//     return (
-//         <Layout
-//             pageMeta={{
-//                 title: "Page not Found | Webdacity",
-//                 description: "Page not Found",
-//                 robots: "noindex, nofollow"
-//             }}
-//         >
-//         </Layout>
-//     )
-// }
+const NotFound = () => {
+    return (
+        <Layout
+            pageMeta={{
+                title: "Page not Found | Art by Jaret",
+                description: "Page not Found",
+                robots: "noindex, nofollow"
+            }}
+        >
+            <h1> Page not Found </h1>
+        </Layout>
+    )
+}
 
-// export default NotFound
+export default NotFound
